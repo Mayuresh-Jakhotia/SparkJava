89be8caf6a8d03349879c66d0a303baff1b697ca
Mayuresh <mayuresh.jakhotia@imanage.com>
2018-03-08 12:24:02 -0600

    Added get and post calls to add/fetch messages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,50 @@
 import static spark.Spark.*;
 
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
 public class Main {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+
+  static HashMap<Integer, Message> messages = new HashMap<Integer, Message>();
+
+  public static void main(String[] args) {
+
+    // Basic Hello World endpoint
+    get("/hello", (req, res) -> "Hello World");
+
+    // Add a message
+    post("/messages", (request, response) -> {
+      return addMessage(new JSONObject(request.body()));
+    });
+
+    // Get message by ID
+    get("/messages/:id", (request, response) -> {
+      if(Integer.parseInt(request.params(":id")) >= messages.size()) {
+        return 404;
+      }
+      
+      Message responseMessage = messages.get(Integer.parseInt(request.params(":id")));
+      JSONObject jsonObj = new JSONObject();
+      jsonObj.put("id", request.params(":id"));
+      jsonObj.put("text", responseMessage.getText());
+      jsonObj.put("from", responseMessage.getFrom());
+      jsonObj.put("to", responseMessage.getTo());
+
+      return jsonObj;
+    });
+  }
+
+  public static JSONObject addMessage(JSONObject jsonObj) {  
+    int id = messages.size();
+
+    jsonObj.put("id", id);
+
+    String text = jsonObj.getString("text");
+    String from = jsonObj.getString("from");
+    String to = jsonObj.getString("to");    
+    messages.put(id, new Message(text, from, to));
+
+    return jsonObj;
+  }
 }
